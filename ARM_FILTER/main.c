4b11458bbe0151825b340269ef3c27cef35dc9f4
LuukBleijendaal <l.bleijendaal@nemsense.nl>
2019-11-18 12:56:36 +0100

    initial commit

---
+++ ARM_FILTER/main.c
@@ -0,0 +1,21 @@
+/*
+ * ARM_FILTER.c
+ *
+ * Created: 18/11/2019 12:56:14
+ * Author : luuk_
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
